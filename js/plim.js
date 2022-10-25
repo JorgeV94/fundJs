6822159186bf0b373f19c3f200260b854a4008e8
Jorge Alves <jorge.vieira.a@hotmail.com>
2022-10-25 15:04:40 -0300

    Mais uma etapa de js

---
+++ js/plim.js
@@ -0,0 +1,26 @@
+
+function myFunction() {
+	let i = 0;
+    
+	for(i = 1; i <= 20; i++){
+    	let resto = 0;
+        let tipo = "impar";
+       
+        resto = i % 2;
+        if (resto == 0){
+        	tipo = "par";
+        }
+        resto = i % 4;
+        if (resto == 0){
+            document.write("plim","-", tipo, "<br>");
+        } else {
+    		document.write(i,"-", tipo, "<br>");
+        }
+    }
+}
+
+function contarItensDaLista(){
+    // tupla cores Registro 
+    const cores = ["azul","amarelo","vermelho","verde","laranja","roxo"];
+    document.getElementById("demo").innerHTML = "A lista tem " + cores.length + " item(s)";
+}
