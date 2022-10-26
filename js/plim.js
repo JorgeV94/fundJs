8cbfd9fbdf8c88b8feeafe00cfae83884160bd0f
Jorge Alves <jorge.vieira.a@hotmail.com>
2022-10-26 17:37:28 -0300

    Adição dia 26/10

--- js/plim.js
+++ js/plim.js
@@ -21,6 +21,54 @@ function myFunction() {
 
 function contarItensDaLista(){
     // tupla cores Registro 
-    const cores = ["azul","amarelo","vermelho","verde","laranja","roxo"];
-    document.getElementById("demo").innerHTML = "A lista tem " + cores.length + " item(s)";
+    //const cores = ["azul","amarelo","vermelho","verde","laranja","roxo","branco","preto","cinza"];
+    //const cores = ["azul"];
+    const cores = [];
+    let mensagem = "";
+    if (cores.length > 1) {
+        mensagem = "A lista tem " + cores.length + " items";
+    } else if(cores.length == 1){
+        mensagem = "A lista tem 1 item";
+    } else {
+        mensagem = "A lista esta vazia ";
+    }
+    
+
+    document.getElementById("demo").innerHTML = mensagem;
+}
+
+function contarItensDaLista2(){
+    // tupla cores Registro 
+    const cores = ["azul","amarelo","vermelho","verde","laranja","roxo","branco","preto","cinza"];
+    //const cores = ["azul"];
+    //const cores = [];
+    let mensagem = "";
+
+    switch (cores.length) {
+        case 0:
+            mensagem = "A lista esta vazia ";
+            break;
+        case 1:
+            mensagem = "A lista tem 1 item";
+            break;
+        default:
+            mensagem = "A lista tem " + cores.length + " items";
+            break;
+    }
+    document.getElementById("demo").innerHTML = mensagem;
+}
+
+function sortearListaCrescente() {
+    const alunos = ["Sara","Abraao","MK","Ademar","Norma","Lene","Beatriz"];
+    alunos.sort();
+    document.getElementById("demo").innerHTML = alunos;
+
+}
+
+function sortearListaDecrescente() {
+    const alunos = ["Sara","Abraao","MK","Ademar","Norma","Lene","Beatriz"];
+    alunos.sort();//ordena o conteudo da lista
+    alunos.reverse();//inverte a lista 
+    document.getElementById("demo").innerHTML = alunos;
+
 }
